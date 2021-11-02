5a09feed9a14c842775f76b46292bffabf07ae13
Jose Sabater <jsm@jsabater.info>
2021-11-02 21:51:29 +0000

    Extend implementation and description

--- src/dataeng.py
+++ src/dataeng.py
@@ -1,6 +1,6 @@
 from io import StringIO
-import json
 import logging
+import operator
 from pathlib import Path
 import sys
 from astropy.table import Table
@@ -11,12 +11,15 @@ import requests
 from validators import url
 
 
+# Configure logging
 logging.basicConfig(
     level=logging.DEBUG, 
     format='%(asctime)s | %(name)s | %(levelname)s | %(message)s'
 )
 log = logging.getLogger(__name__)
 
+
+# Step 1
 class Extractor:
     """Class to perform some validation, and load the CSV in memory. It can also "extract" the data if an URL is provided.
     """
@@ -36,7 +39,7 @@ class Extractor:
         
         # Get columns and verify
         log.debug(f"Verify that the columns are in the header")
-        self.header_columns = (e.strip() for e in self.header.split(","))
+        self.header_columns = (e.strip().lower() for e in self.header.split(","))
         if check_columns is not None:
             for column in check_columns:
                 if column not in self.header_columns:
@@ -47,7 +50,7 @@ class Extractor:
             self.load_data()
         
     def load_data(self):
-        """Load data into table in memory"""
+        """Load data into an structured array table in memory"""
         # Actually load the data
         if self.location_type in ["url", "file"]:
             log.info("Start data loading")
@@ -100,25 +103,66 @@ def location_type(location):
     else:
         return "unknown"
 
+# Step 2
+class Cleaner:
+    """Class to clean in a reproducible way the metadata and data"""
+    def __init__(self, data):
+        self.data = data
+        # Apply cleaning of the header
+        # TODO: Implement
 
-def compute_quantile(data_column, quantile=0.9):
-    log.debug(f"Start compute quantile {quantile}")
-    q = np.quantile(data_column, quantile)
-    log.debug(f"Quantile result for column: {q}")
-    return q
+# Step 3
+class Compute:
+    """Class to apply the statistical computations and filter the data"""
+    def __init__(self, data, column="trip_distance", quantile=0.9, operation=operator.ge):
+        # Compute
+        log.debug(f"Start compute quantile {quantile} for {column}")
+        self.quantile = self.compute_quantile(data[column], quantile=quantile)
+        log.debug(f"Quantile result for column {column}: {self.quantile}")
+        # Filter
+        self.data = data[operation(data[column], self.quantile)]
+        log.debug("Filtering applied")
+    
+    @staticmethod
+    def compute_quantile(data_column, quantile=0.9):
+        """Small wrapper around the Numpy quantile function"""
+        return np.quantile(data_column, quantile)
+        
+
+# Step 4
+class Output:
+    """Class to output the data in the desired format"""
+    def __init__(self, data, format="csv", output=sys.stdout):
+        """Outputs the data in the desired format in the given output"""
+        self.data = data
+        log.debug(f"Start data output in format {format}")
+        self.data.write(output, format=format)
+        log.debug("End data output")
+
+
+# Pipeline: Integration of all the steps
+def pipeline(filename_or_url, check_columns=("trip_distance",), output_format="csv", quantile=0.9, output=sys.stdout):
+    """Run the pipeline.
+
+    For clarity the steps are not chained
+    """
+    try:
+        data = Extractor(filename_or_url, check_columns=check_columns).data
+        clean_data = Cleaner(data).data
+        filtered_data = Compute(clean_data, quantile=quantile).data
+        Output(filtered_data, format=output_format, output=output)
+        log.info("Status: OK")
+    except Exception as e:
+        # Exceptions are logged and then raised
+        log.info("Status: Failed")
+        log.info(f"Exception: {repr(e)}")
+        raise e
+    
 
 
 @click.command()
 @click.argument("filename")
 def cli(filename):
-    try:
-        data = Extractor(filename, check_columns=["trip_distance"]).data
-        result = compute_quantile(data["trip_distance"])
-        log.info("Status: OK")
-        data[data["trip_distance"] >= result].write(sys.stdout, format='csv')
-        
-    except Exception as e:
-        log.info("Status: Failed")
-        log.info(f"Exception: {repr(e)}")
-        raise e
+    """Function to wrap the pipeline with a CLI"""
+    pipeline(filename)
 
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -9,10 +9,12 @@ BASE_DIR = Path(__file__).resolve().parents[1]
 TEST_STORAGE_PATH = Path(BASE_DIR, "tests", "data")
 CSV1 = TEST_STORAGE_PATH / Path("yellow_tripdata_2014-08-small.csv")
 CSV2 = TEST_STORAGE_PATH / Path("yellow_tripdata_2020-01-small.csv")
+CSV3 = TEST_STORAGE_PATH / Path("yellow_tripdata_2009-09-small.csv")
 CSV1_HEADER = "vendor_id, pickup_datetime, dropoff_datetime, passenger_count, trip_distance, pickup_longitude, pickup_latitude, rate_code, store_and_fwd_flag, dropoff_longitude, dropoff_latitude, payment_type, fare_amount, surcharge, mta_tax, tip_amount, tolls_amount, total_amount"
 CSV2_HEADER = "VendorID,tpep_pickup_datetime,tpep_dropoff_datetime,passenger_count,trip_distance,RatecodeID,store_and_fwd_flag,PULocationID,DOLocationID,payment_type,fare_amount,extra,mta_tax,tip_amount,tolls_amount,improvement_surcharge,total_amount,congestion_surcharge"
 DIST1 = np.array([2.7000000000000002, 20.399999999999999, 2.1000000000000001, 1.3, 1.7, 1.7, 1, 9.1999999999999993, 2.6000000000000001, 1.3999999999999999, 3.2000000000000002, 7.7999999999999998, 1.1000000000000001, 3.2999999999999998, 5.2999999999999998, 6.2000000000000002, 15.6,  0.90000000000000002, 1.3999999999999999])
 DIST2 = np.array([1.20, 1.20, .60, .80, .00, .03, .00, .00, .00, .70, 2.40, .80, 3.30, 1.07, 7.76, 1.60, .50, 1.70, 8.45, .00])
+DIST3 = np.array([10.23,  0.62,  0.96,  4.25,  0.99,  1.78,  2.16,  3.38,  7.75, 4.46,  3.13,  0.68,  4.5 ,  1.83,  1.53,  0.76,  4.83,  1.28])
 URL_CSV1 = "https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2014-08.csv"
 URL_CSV2 = "https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2020-01.csv"
 
@@ -21,11 +23,21 @@ def test_load_local_csvs():
     Extractor(CSV1)
     Extractor(CSV2)
 
-def test_retrieve_trip_distance_column():
-    result1 = Extractor(CSV1, check_columns=["trip_distance"]).data
-    result2 = Extractor(CSV2, check_columns=["trip_distance"]).data
-    nt.assert_array_equal(result1["trip_distance"], DIST1)
-    nt.assert_array_equal(result2["trip_distance"], DIST2)
+def test_retrieve_trip_distance_column_format1():
+    """This format required the stripping of the header column names"""
+    result = Extractor(CSV1, check_columns=["trip_distance"]).data
+    nt.assert_array_equal(result["trip_distance"], DIST1)
+
+def test_retrieve_trip_distance_column_format2():
+    """This format is OK"""
+    result = Extractor(CSV2, check_columns=["trip_distance"]).data
+    nt.assert_array_equal(result["trip_distance"], DIST2)
+
+def test_retrieve_trip_distance_column_format3():
+    """This format required converting to lower case the header column names"""
+    result = Extractor(CSV3, check_columns=["trip_distance"]).data
+    nt.assert_array_equal(result["Trip_Distance"], DIST3)
+
 
 def test_location_type_url():
     assert location_type(URL_CSV1) == "url"
